ac623c5babe0c803bb0a61a1c215033faeec15ca
parithosh <parithosh.jayanthi@ethereum.org>
2025-07-08 11:49:42 +0200

    adding perf-devnet-2

---
+++ terraform/devnet-2/dns.tf
@@ -0,0 +1,90 @@
+////////////////////////////////////////////////////////////////////////////////////////
+//                                   DNS CONFIGURATION
+////////////////////////////////////////////////////////////////////////////////////////
+
+data "cloudflare_zone" "default" {
+  name = "ethpandaops.io"
+}
+
+locals {
+  # DNS records data extracted from inventory
+  dns_hosts = {
+    "bootnode-1" = {
+      ipv4 = "157.180.14.229"
+      ipv6 = "2a01:4f9:3100:37a3::2"
+    }
+    "lighthouse-nethermind-1" = {
+      ipv4 = "157.180.14.230"
+      ipv6 = "2a01:4f9:3100:37aa::2"
+    }
+    "lighthouse-reth-1" = {
+      ipv4 = "157.180.14.227"
+      ipv6 = "2a01:4f9:3100:37a0::2"
+    }
+    "nimbus-besu-1" = {
+      ipv4 = "157.180.14.226"
+      ipv6 = "2a01:4f9:3100:37af::2"
+    }
+    "prysm-geth-1" = {
+      ipv4 = "157.180.14.225"
+      ipv6 = "2a01:4f9:3100:379f::2"
+    }
+    "teku-erigon-1" = {
+      ipv4 = "157.180.14.228"
+      ipv6 = "2a01:4f9:3100:37a1::2"
+    }
+  }
+
+  # Generate DNS records for each host and subdomain
+  dns_records = flatten([
+    for hostname, ips in local.dns_hosts : [
+      # Main hostname A and AAAA records
+      {
+        name    = "${hostname}.${var.ethereum_network}"
+        type    = "A"
+        content = ips.ipv4
+      },
+      {
+        name    = "${hostname}.${var.ethereum_network}"
+        type    = "AAAA"
+        content = ips.ipv6
+      },
+      # Beacon node (bn) subdomain
+      {
+        name    = "bn.${hostname}.${var.ethereum_network}"
+        type    = "A"
+        content = ips.ipv4
+      },
+      {
+        name    = "bn.${hostname}.${var.ethereum_network}"
+        type    = "AAAA"
+        content = ips.ipv6
+      },
+      # RPC subdomain
+      {
+        name    = "rpc.${hostname}.${var.ethereum_network}"
+        type    = "A"
+        content = ips.ipv4
+      },
+      {
+        name    = "rpc.${hostname}.${var.ethereum_network}"
+        type    = "AAAA"
+        content = ips.ipv6
+      }
+    ]
+  ])
+}
+
+# Create DNS records for static OVH hosts
+resource "cloudflare_record" "ovh_static_hosts" {
+  for_each = {
+    for idx, record in local.dns_records : "${record.name}-${record.type}" => record
+  }
+
+  zone_id = data.cloudflare_zone.default.id
+  name    = each.value.name
+  value   = each.value.content
+  type    = each.value.type
+  ttl     = 120
+  proxied = false
+}
---
+++ terraform/devnet-2/main.tf
@@ -0,0 +1,54 @@
+////////////////////////////////////////////////////////////////////////////////////////
+//                            TERRAFORM PROVIDERS & BACKEND
+////////////////////////////////////////////////////////////////////////////////////////
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.28"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.42.1"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    endpoints                   = { s3 = "https://fra1.digitaloceanspaces.com" }
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    region                      = "us-east-1"
+    bucket                      = "merge-testnets"
+    key                         = "infrastructure/perf-devnet-2/terraform.tfstate"
+  }
+}
+
+provider "digitalocean" {
+  http_retry_max = 20
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//                                        VARIABLES
+////////////////////////////////////////////////////////////////////////////////////////
+variable "cloudflare_api_token" {
+  type        = string
+  sensitive   = true
+  description = "Cloudflare API Token"
+}
+
+variable "ethereum_network" {
+  type    = string
+  default = "perf-devnet-2"
+}
